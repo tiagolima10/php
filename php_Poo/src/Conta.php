17e6607a0b17696dd8f26166d0370fa2329510ba
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2023-10-01 11:29:43 -0300

    v1.0

--- php_Poo/src/Conta.php
+++ php_Poo/src/Conta.php
@@ -9,7 +9,7 @@ class Conta
 
 /*
 $Conta1 = new Conta;
-$Conta1 -> nomeTitular = 'Cavalo Lima';
+$Conta1 -> nomeTitular = 'Mano Lima';
 $Conta1 -> cpfTitular = '987.654.321-01';
 $Conta1 -> saldo = 1000;
 */
